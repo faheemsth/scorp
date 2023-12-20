2b39944b9950ba1c5d266deeebe83fe80fa50874
Muhammad Kashif <muhammadkashif70000@gmail.com>
2023-12-20 14:39:03 +0500

    mass update

--- resources/views/deals/task_details.blade.php
+++ resources/views/deals/task_details.blade.php
@@ -580,48 +580,50 @@
                                                             </span>
                                                             <div class="card-body px-0">
                                                                 <ul class="list-group list-group-flush mt-2">
-
-                                                                    @foreach ($discussions as $discussion)
-                                                                        <li class="list-group-item px-3"
-                                                                            id="lihover">
-                                                                            <div
-                                                                                class="d-block d-sm-flex align-items-start">
-                                                                                <img src="{{ asset('assets/images/user/avatar.png') }}"
-                                                                                    class="img-fluid wid-40 me-3 mb-2 mb-sm-0"
-                                                                                    alt="image">
-                                                                                <div class="w-100">
-                                                                                    <div
-                                                                                        class="d-flex align-items-center justify-content-between">
-                                                                                        <div class="mb-3 mb-sm-0">
-                                                                                            <h5 class="mb-0">
-                                                                                                {{ $discussion['comment'] }}
-                                                                                            </h5>
-                                                                                            <span
-                                                                                                class="text-muted text-sm">{{ $discussion['name'] }}</span>
-                                                                                        </div>
+                                                                    @if($discussions != null)
+                                                                        @foreach ($discussions as $discussion)
+                                                                            <li class="list-group-item px-3"
+                                                                                id="lihover">
+                                                                                <div
+                                                                                    class="d-block d-sm-flex align-items-start">
+                                                                                    <img src="{{ asset('assets/images/user/avatar.png') }}"
+                                                                                        class="img-fluid wid-40 me-3 mb-2 mb-sm-0"
+                                                                                        alt="image">
+                                                                                    <div class="w-100">
                                                                                         <div
-                                                                                            class=" form-switch form-switch-right ">
-                                                                                            {{ $discussion['created_at'] }}
-                                                                                        </div>
-                                                                                        <div class="d-flex gap-3"
-                                                                                            id="dellhover">
-                                                                                            <i class="ti ti-pencil textareaClassedit"
-                                                                                                data-comment="{{ $discussion['comment'] }}"
-                                                                                                data-id="{{ $discussion['id'] }}"
-                                                                                                id="editable"
-                                                                                                style="font-size: 20px;"></i>
-                                                                                            <script></script>
-                                                                                            <i class="ti ti-trash"
-                                                                                                id="editable"
-                                                                                                style="font-size: 20px;"
-                                                                                                onclick="DeleteComment('{{ $discussion['id'] }}','{{ $task->id }}')"></i>
+                                                                                            class="d-flex align-items-center justify-content-between">
+                                                                                            <div class="mb-3 mb-sm-0">
+                                                                                                <h5 class="mb-0">
+                                                                                                    {{ $discussion['comment'] }}
+                                                                                                </h5>
+                                                                                                <span
+                                                                                                    class="text-muted text-sm">{{ $discussion['name'] }}</span>
+                                                                                            </div>
+                                                                                            <div
+                                                                                                class=" form-switch form-switch-right ">
+                                                                                                {{ $discussion['created_at'] }}
+                                                                                            </div>
+                                                                                            <div class="d-flex gap-3"
+                                                                                                id="dellhover">
+                                                                                                <i class="ti ti-pencil textareaClassedit"
+                                                                                                    data-comment="{{ $discussion['comment'] }}"
+                                                                                                    data-id="{{ $discussion['id'] }}"
+                                                                                                    id="editable"
+                                                                                                    style="font-size: 20px;"></i>
+                                                                                                <script></script>
+                                                                                                <i class="ti ti-trash"
+                                                                                                    id="editable"
+                                                                                                    style="font-size: 20px;"
+                                                                                                    onclick="DeleteComment('{{ $discussion['id'] }}','{{ $task->id }}')"></i>
+                                                                                            </div>
                                                                                         </div>
                                                                                     </div>
                                                                                 </div>
-                                                                            </div>
-                                                                        </li>
-                                                                    @endforeach
-
+                                                                            </li>
+                                                                        @endforeach
+                                                                    @else
+                                                                        <li class="list-group-item px-3" style="text-align:center">No comments found!</li>
+                                                                    @endif
                                                                 </ul>
                                                             </div>
                                                         </div>
