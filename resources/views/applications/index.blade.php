d3a29a2b1047b4958c2a27abb56388601c155762
faheemsth <ggta142@gmail.com>
2023-12-22 19:08:34 +0500

    updated code

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -139,6 +139,7 @@ $profile=\App\Models\Utility::get_file('uploads/avatar/');
                         <th style="width: 50px !important;">
                             <input type="checkbox" class="main-check">
                         </th>
+                        <th scope="col">{{ __('Name') }}</th>
                         <th scope="col">{{ __('University') }}</th>
                         <th scope="col">{{ __('Intake') }}</th>
                         <th scope="col">{{ __('Brand') }}</th>
@@ -160,11 +161,16 @@ $profile=\App\Models\Utility::get_file('uploads/avatar/');
                                  <td>
                                     <input type="checkbox" name="applications[]" value="{{$app->id}}" class="sub-check">
                                 </td>
+                                <td>
+                                <span style="cursor:pointer" class="hyper-link" @can('view application') onclick="openSidebar('deals/'+{{ $app->id }}+'/detail-application')" @endcan>
+                                {{ strlen($app->name) > 10 ? substr($app->name, 0, 10) . '...' : $app->name }}
+                            </span>
+                                </td>
                                 <td>{{ $universities[$app->university_id] }}</td>
                                 <td> {{ $app->intake }} </td>
                                 <td> {{ isset($users[$deal->brand_id]) ? $users[$deal->brand_id] : '' }}  </td>
                                 <td> {{ isset($branch->name) ? $branch->name : ''  }} </td>
-                                <td> {{ $users[$deal->assigned_to] }} </td>
+                                <td> {{ !empty($deal->assigned_to) ? (isset($users[$deal->assigned_to]) ? $users[$deal->assigned_to] : '') : '' }} </td>
                                 <td>{{ isset($app->stage_id) && isset($stages[$app->stage_id]) ? $stages[$app->stage_id] : '' }}</td>
                             </tr>
                         @empty
