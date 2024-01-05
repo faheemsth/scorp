99d1194aa5c17c2ea24a6affb5073e18e0be701a
sherazaleem <sherazaleem015@gmail.com>
2024-01-05 18:21:03 +0500

    update

--- resources/views/chartOfAccount/TestChart.blade.php
+++ resources/views/chartOfAccount/TestChart.blade.php
@@ -6,7 +6,7 @@
     <li class="breadcrumb-item"><a href="{{ route('crm.dashboard') }}">{{ __('Dashboard') }}</a></li>
     <li class="breadcrumb-item">{{ __('Leads') }}</li>
 @endsection
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 <style>
     .action-btn {
         display: inline-grid !important;
@@ -33,6 +33,20 @@
     .form-control:focus {
         border: 1px solid rgb(209, 209, 209) !important;
     }
+
+    .recoredtext {
+        background-color: white !important;
+    }
+
+    .recoredtext span {
+
+        color: blue !important;
+    }
+
+    .dropdown-menu:focus {
+        box-shadow: none;
+        outline: none
+    }
 </style>
 
 
@@ -40,106 +54,137 @@
 
 
 
-<style>
-    /*------------------------------
-    Template name: Bootstrap 5 Center logo
-    Author name: Md Enjamul Islam
-    Author URI: https://github.com/mdenjamulislam
---------------------------------*/
-
-body {
-    font-family: cursive;
-    font-size: 18px;
-    font-weight: 400;
-    line-height: 30px;
-    color: #585858;
-}
-.navbar {
-    padding: 15px 0;
-}
-.navbar-brand {
-
-}
-.navbar a {
-    text-decoration: none;
-}
-h2.logo {
-    font-size: 28px;
-    font-weight: 400;
-    color: #fff;
-}
-h2.logo strong {
-    font-size: 35px;
-    color: #EDF828;
-    font-weight: 600;
-}
-h2.center-logo {
-    margin: 0 25px;
-}
-#navbarSupportedContent {
-    justify-content: center;
-}
-ul.navbar-nav {}
-ul.navbar-nav li.nav-item {
-    margin-right: 25px;
-}
-ul.navbar-nav li.nav-item:last-child {
-    margin-right: 0;
-}
-ul.navbar-nav li.nav-item a.nav-link {
-    color: #fff;
-}
-.navbar-light .navbar-nav .nav-link.active, .navbar-light .navbar-nav .show>.nav-link {
-    color: #EDF828;
-}
-ul.navbar-nav li.nav-item a.nav-link:hover {
-    color: #EDF828;
-}
-</style>
 @section('content')
     <div class="row">
         <div class="col-xl-12">
-            <div class="card my-card" style="max-width: 98%;border-radius:0px; min-height: 250px !important;">
-                <div class="card card-header">
-                    <div id="header">
-                        <div class="container">
-                            <nav class="navbar navbar-expand-lg navbar-dark">
-                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <ul class="navbar-nav">
-                                        <li class="nav-item">
-                                            <a class="nav-link">
-                                               <div class="row d-column">
-                                                <div class="col-md-6"><i class="ti ti-pencil"></i></div>
-                                                <div class="col-md-6">Visa Analysis</div>
-                                               </div>
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link">
-                                                <div class="row">
-                                                    <div class="col-md-6"><i class="ti ti-pencil"></i></div>
-                                                    <div class="col-md-6">Visa Analysis</div>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link">
-                                                <div class="row">
-                                                    <div class="col-md-6"><i class="ti ti-pencil"></i></div>
-                                                    <div class="col-md-6">Visa Analysis</div>
-                                                </div>
-                                            </a>
-                                        </li>
-                                    </ul>
+
+            <div class="container">
+                <div class="row justify-content-center align-items-center my-4">
+                    <div class="col-3">
+                        <div class="row justify-content-center align-items-center">
+                            <div class="col-auto d-flex flex-column gap-2 justify-content-center align-items-center">
+                                <div style="border: 2px solid black; border-radius: 50%;text-align: center;width: 25px;height: 25px;"
+                                    class="p-1 mb-1 ">
+                                    <i class="fa-solid fa-dollar-sign" style="color:black"></i>
                                 </div>
-                            </nav>
+                                <h6 class="fw-bold">Visas Analysis</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="row justify-content-center align-items-center">
+                            <div class="col-auto d-flex flex-column gap-2 justify-content-center align-items-center">
+                                <div style="border: 2px solid black; border-radius: 50%;text-align: center;width: 25px;height: 25px;"
+                                    class="p-1 mb-1">
+                                    <i class="fa-solid fa-dollar-sign" style="color:black"></i>
+                                </div>
+                                <h6 class="fw-bold">Deposits Analysis</h6>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-3">
+                        <div class="row justify-content-center align-items-center">
+                            <div class="col-auto d-flex flex-column gap-2 justify-content-center align-items-center">
+                                <div style="border: 2px solid black; border-radius: 50%;text-align: center;width: 25px;height: 25px;"
+                                    class="p-1 mb-1">
+                                    <i class="fa-solid fa-dollar-sign" style="color:black"></i>
+                                </div>
+                                <h6 class="fw-bold">Application Analysis</h6>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+            </div>
+            <div class="d-flex justify-content-center align-items-center py-2"
+                style="background-color: #1F2735;color: white">
+                <div class="col-2 text-center">
+                    <p class="mb-0">Record Count</p>
+                    <h4 class="text-white">6,224</h4>
+                </div>
+                <div class="col-auto">
+                    <div class="dropdown">
+                        <button class="btn recoredtext dropdown-toggle" type="button" id="dropdownMenuButton1"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <span> Brands</span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="dropdown">
+                        <button class="btn recoredtext dropdown-toggle" type="button" id="dropdownMenuButton1"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <span>Locations</span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="dropdown">
+                        <button class="btn recoredtext dropdown-toggle" type="button" id="dropdownMenuButton1"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <span> Instition</span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="dropdown">
+                        <button class="btn recoredtext dropdown-toggle" type="button" id="dropdownMenuButton1"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <span>Intakes</span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="dropdown">
+                        <button class="btn recoredtext dropdown-toggle" type="button" id="dropdownMenuButton1"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <span> Deposit Date</span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-3">
                 <div class="card-body table-border-style" style="padding: 25px 3px;">
-                    <div class="row align-items-center ps-0 ms-0 pe-4 my-2">
-                        <div class="col-4">
-                            sds
+                    <div class="row align-items-center justify-content-center ps-0 ms-0 pe-4 my-2">
+                        <div class="col-12">
+                            <canvas id="FirstChart" style="width:100%;max-width:600px"></canvas>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-3">
+                <div class="card-body table-border-style" style="padding: 25px 3px;">
+                    <div class="row align-items-center justify-content-center ps-0 ms-0 pe-4 my-2">
+                        <div class="col-12">
+                            <canvas id="SecondChart" style="width:100%;max-width:600px"></canvas>
+
                         </div>
                     </div>
                 </div>
@@ -148,4 +193,81 @@ ul.navbar-nav li.nav-item a.nav-link:hover {
     </div>
 @endsection
 @push('script-page')
+    <script>
+        const xValFirst = ["Italy", "France", "Spain", "USA", "Argentina", "France", "Spain", "USA", "Argentina"];
+        const yValFirst = [55, 49, 44, 24, 15, 24, 15, 25, 105, 35];
+        const barColorFirst = ["#1F2735", "#1F2735", "#1F2735", "#1F2735", "#1F2735"];
+
+        new Chart("FirstChart", {
+            type: "bar",
+            data: {
+                labels: xValFirst,
+                datasets: [{
+                    backgroundColor: barColorFirst,
+                    data: yValFirst
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: "World Wine Production 2018"
+                }
+            }
+        });
+    </script>
+    <script>
+        const xValues = ["Italy", "France", "Spain", "USA", "Argentina", "Argentina", "Argentina", "Argentina"];
+        const yValues = [55, 49, 44, 24, 15, 25, 105, 35];
+        const barColors = ["#1F2735", "#1F2735", "#1F2735", "#1F2735", "#1F2735", "#1F2735", "#1F2735", "#1F2735"];
+
+        new Chart("SecondChart", {
+            type: "horizontalBar",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    backgroundColor: barColors,
+                    data: yValues
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: "World Wine Production 2018"
+                },
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                },
+                layout: {
+                    padding: {
+                        left: 50, // Adjust left padding to make space for bar colors
+                        right: 50,
+                        top: 0,
+                        bottom: 0
+                    }
+                },
+                plugins: {
+                    datalabels: {
+                        anchor: 'end', // Anchor labels at the end of bars
+                        align: 'end',
+                        color: 'black' // Color of the labels
+                    }
+                }
+            }
+        });
+    </script>
 @endpush
