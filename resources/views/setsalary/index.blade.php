e0a9ac9463f6be14b20dc927e99d13e3431b1ab3
mubashersaleem786 <164843405+mubashersaleem786@users.noreply.github.com>
2024-03-25 16:19:30 +0500

    improved frontend of set salary

--- resources/views/setsalary/index.blade.php
+++ resources/views/setsalary/index.blade.php
@@ -161,7 +161,7 @@
                                 @foreach ($employees as $employee)
                                 <tr>
                                     <td class="Id">
-                                        <a href="{{route('setsalary.show',$employee->id)}}" class="btn btn-outline-primary" data-toggle="tooltip" data-original-title="{{__('View')}}">
+                                        <a href="{{route('setsalary.show',$employee->id)}}" class="btn btn-outline-dark" data-toggle="tooltip" data-original-title="{{__('View')}}">
                                             {{ \Auth::user()->employeeIdFormat($employee->employee_id) }}
                                         </a>
                                     </td>
@@ -170,11 +170,12 @@
                                     <td>{{ \Auth::user()->priceFormat($employee->salary) }}</td>
                                     <td>{{ !empty($employee->get_net_salary()) ?\Auth::user()->priceFormat($employee->get_net_salary()):'' }}</td>
                                     <td>
-                                        <div class="action-btn bg-success ms-2">
-                                            <a href="{{route('setsalary.show',$employee->id)}}" class="mx-3 btn btn-sm align-items-center" data-bs-toggle="tooltip" title="{{__('Set Salary')}}" data-original-title="{{__('View')}}">
-                                                <i class="ti ti-eye text-white"></i>
-                                            </a>
-                                        </div>
+   <div class="action-btn bg-dark ms-2" style="color:white; width:36px; height: 36px; margin-top:10px;">
+    <a href="{{ route('setsalary.show', $employee->id) }}" class="mx-3 btn btn-sm align-items-center" data-bs-toggle="tooltip" title="{{ __('Set Salary') }}" data-original-title="{{ __('View') }}">
+        <i class="ti ti-eye text-white" style="font-size: 18px;"></i>
+    </a>
+</div>
+
                                     </td>
                                 </tr>
                                 @endforeach
